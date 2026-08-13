f680fe36a78b69d7f527e683dbfa191ad359e9ae
aitpit2port <ait.pit2port@info-ait.com>
2026-08-13 14:38:09 +0300

    Update backend-config.js

--- backend-config.js
+++ backend-config.js
@@ -1,5 +1,5 @@
 window.AIT_BACKEND_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbxSP0Nd7K9a3QLgC1GE8ZOZW6PlHliS0v7eYnV-J9Ku0JvxtOJuZU-d85xWNJUSQhJu/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxSP0Nd7K9a3QLgC1GE8ZOZW6PlHliS0v7eYnV-J9Ku0JvxtOJuZU-d85xWNJUSQhJu/exec', 
   githubOrigin: window.location.origin,
   loginWidth: 480,
   loginHeight: 640,
