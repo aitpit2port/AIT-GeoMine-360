65498544e1c8a38af31f48c2d3f220ec5944ce08
aitpit2port <ait.pit2port@info-ait.com>
2026-08-13 23:06:03 +0300

    Add files via upload

---
+++ backend-config.js
@@ -0,0 +1,7 @@
+window.AIT_BACKEND_CONFIG = {
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwhw9X298uc-xoyKPnsrypjoXpEfwhDvPeBIZahNst3O_1QzloE5R_bKUZ7lM62rl1K/exec',
+  githubOrigin: 'https://aitpit2port.github.io',
+  loginWidth: 480,
+  loginHeight: 640,
+  tokenStorageKey: 'ait_geomine_session_v2'
+};
