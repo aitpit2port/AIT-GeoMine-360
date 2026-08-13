247b56cfcf8146437d705a4c61433a20b964970e
aitpit2port <ait.pit2port@info-ait.com>
2026-08-13 15:27:33 +0300

    Update backend-config.js

--- backend-config.js
+++ backend-config.js
@@ -1,6 +1,6 @@
 window.AIT_BACKEND_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbwhw9X298uc-xoyKPnsrypjoXpEfwhDvPeBIZahNst3O_1QzloE5R_bKUZ7lM62rl1K/exec', 
-  githubOrigin: window.location.origin,
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwhw9X298uc-xoyKPnsrypjoXpEfwhDvPeBIZahNst3O_1QzloE5R_bKUZ7lM62rl1K/exec',
+  githubOrigin: 'https://aitpit2port.github.io',
   loginWidth: 480,
   loginHeight: 640,
   tokenStorageKey: 'ait_geomine_session_v2'
